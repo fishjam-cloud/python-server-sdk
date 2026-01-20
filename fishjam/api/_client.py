b8016b7e622ade4894c7cfd183160f47540100a6
Bernard Gawor <gaworbernard@gmail.com>
2026-01-20 14:37:41 +0100

    add headers to _client.py since client.py is autogenerated

--- fishjam/api/_client.py
+++ fishjam/api/_client.py
@@ -5,12 +5,13 @@ from fishjam._openapi_client.models import Error
 from fishjam._openapi_client.types import Response
 from fishjam.errors import HTTPError
 from fishjam.utils import get_fishjam_url
+from fishjam.version import get_version
 
 
 class Client:
     def __init__(self, fishjam_id: str, management_token: str):
         self._fishjam_url = get_fishjam_url(fishjam_id)
-        self.client = AuthenticatedClient(self._fishjam_url, token=management_token)
+        self.client = AuthenticatedClient(self._fishjam_url, token=management_token, headers={"x-fishjam-api-client": f"python-server-{get_version()}"})
 
     def _request(self, method, **kwargs):
         response = method.sync_detailed(client=self.client, **kwargs)
