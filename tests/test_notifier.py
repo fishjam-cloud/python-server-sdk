3383dca877742b468ea897f919df00104f632c02
Przemysław Rożnawski <roznawski.przemyslaw@gmail.com>
2026-05-06 00:44:27 +0200

    Increase webhook timeout

--- tests/test_notifier.py
+++ tests/test_notifier.py
@@ -184,6 +184,7 @@ class TestReceivingNotifications:
             ServerMessageRoomCreated,
             ServerMessagePeerAdded,
             ServerMessagePeerConnected,
+            ServerMessagePeerDisconnected,
             ServerMessagePeerDeleted,
             ServerMessageRoomDeleted,
         ]
@@ -218,7 +219,7 @@ class TestReceivingNotifications:
 
         self.assert_webhook_events(event_checks, event_queue, room.id)
 
-    def assert_webhook_events(self, event_checks, event_queue, room_id, timeout=15):
+    def assert_webhook_events(self, event_checks, event_queue, room_id, timeout=60):
         deadline = time.monotonic() + timeout
         received = []
 
