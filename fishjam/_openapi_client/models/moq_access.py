02d8dbc050eddb640416c6069fce7fb00858ebd0
Karol Konkol <konkolisz@gmail.com>
2026-06-23 12:14:34 +0200

    Update moq api

--- fishjam/_openapi_client/models/moq_access.py
+++ fishjam/_openapi_client/models/moq_access.py
@@ -6,28 +6,35 @@ from typing import Any, TypeVar
 from attrs import define as _attrs_define
 from attrs import field as _attrs_field
 
-T = TypeVar("T", bound="MoqToken")
+T = TypeVar("T", bound="MoqAccess")
 
 
 @_attrs_define
-class MoqToken:
+class MoqAccess:
     """Connection details for a MoQ relay client
 
     Attributes:
-        url (str): Relay connection URL with the JWT embedded as a `?jwt=` query parameter. Pass directly to a MoQ
-            client SDK. Example: https://relay.fishjam.io/abc123?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9....
+        connection_url (str): Relay connection URL with the JWT embedded as a `?jwt=` query parameter. Pass directly to
+            a MoQ client SDK. Example: https://relay.fishjam.io/abc123?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9....
+        token (str): JWT authorizing the MoQ relay connection, also embedded in `connection_url` Example: eyJhbGciOiJIUz
+            I1NiIsInR5cCI6IkpXVCJ9.eyJyb290IjoiZmlzaGphbSIsInB1dCI6WyJteS1zdHJlYW0iXSwiZ2V0IjpbXSwiaWF0IjoxNzEzMzYwMDAwLCJle
+            HAiOjE3MTMzNjM2MDB9.abc123.
     """
 
-    url: str
+    connection_url: str
+    token: str
     additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
 
     def to_dict(self) -> dict[str, Any]:
-        url = self.url
+        connection_url = self.connection_url
+
+        token = self.token
 
         field_dict: dict[str, Any] = {}
         field_dict.update(self.additional_properties)
         field_dict.update({
-            "url": url,
+            "connection_url": connection_url,
+            "token": token,
         })
 
         return field_dict
@@ -35,14 +42,17 @@ class MoqToken:
     @classmethod
     def from_dict(cls: type[T], src_dict: Mapping[str, Any]) -> T:
         d = dict(src_dict)
-        url = d.pop("url")
+        connection_url = d.pop("connection_url")
 
-        moq_token = cls(
-            url=url,
+        token = d.pop("token")
+
+        moq_access = cls(
+            connection_url=connection_url,
+            token=token,
         )
 
-        moq_token.additional_properties = d
-        return moq_token
+        moq_access.additional_properties = d
+        return moq_access
 
     @property
     def additional_keys(self) -> list[str]:
